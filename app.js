061119eafd74b44f5532d507013cba81c4d657cb
21andrewch <21andrewch@students.harker.org>
2019-10-21 10:27:02 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+
+
 var note1 = document.getElementById("note-1");
 var note2 = document.getElementById("note-2");
 var note3 = document.getElementById("note-3");
@@ -18,6 +21,8 @@ var clear = document.getElementById("clear");
 var textbox = document.getElementById("textbox");
 var newnote = document.getElementById("newnote");
 
+
+
 var readyToDelete1 = false;
 var readyToDelete2 = false;
 var readyToDelete3 = false;
@@ -30,18 +35,79 @@ var deleteNote = false;
 var change = false;
 var readyToChange = false;
 
-
-
-addEventListener("keydown", function()
-{
-    if(event.keyCode = 49)
-    change = true;
-
-    if(change && readyToChange)
+function setCookie(cname,cvalue,exdays)
+ {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) 
+  {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++)
     {
-        increaseFontSizeBy1px();
+      var c = ca[i];
+      while (c.charAt(0) == ' ')
+      {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0)
+       {
+        return c.substring(name.length, c.length);
+       }
     }
-})
+    return "";
+  }
+  
+function checkCookie()
+{
+    var newnote1=getCookie("info1");
+    var noteone = document.createElement("li");
+    noteone.innerHTML = newnote1;
+    note1.appendChild(noteone);
+    
+    var newnote2=getCookie("info2");
+    var notetwo = document.createElement("li");
+    notetwo.innerHTML = newnote2;
+    note2.appendChild(notetwo);
+
+    var newnote3=getCookie("info3");
+    var notethree = document.createElement("li");
+    notethree.innerHTML = newnote3;
+    note3.appendChild(notethree);
+
+    var newnote4=getCookie("info4");
+    var notefour = document.createElement("li");
+    notefour.innerHTML = newnote4;
+    note4.appendChild(notefour);
+
+    var newnote5=getCookie("info5");
+    var notefive = document.createElement("li");
+    notefive.innerHTML = newnote5;
+    note5.appendChild(notefive);
+
+    var newnote6=getCookie("info6");
+    var notesix = document.createElement("li");
+    notesix.innerHTML = newnote6;
+    note6.appendChild(notesix);
+
+    var newnote7=getCookie("info7");
+    var notesev = document.createElement("li");
+    notesev.innerHTML = newnote7;
+    note7.appendChild(notesev);
+
+    var newnote8=getCookie("info8");
+    var noteight = document.createElement("li");
+    noteight.innerHTML = newnote8;
+    note8.appendChild(noteight);
+
+}   
+
+
 
 function dropDown() 
 {
@@ -50,17 +116,25 @@ function dropDown()
 
 window.onclick = function(event) 
 {
-    if (!event.target.matches('.help')) {
+    if (!event.target.matches('.help')) 
+    {
       var dropdowns = document.getElementsByClassName("help-box");
       var i;
-      for (i = 0; i < dropdowns.length; i++) {
+      for (i = 0; i < dropdowns.length; i++) 
+      {
         var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
+        if (openDropdown.classList.contains('show')) 
+        {
           openDropdown.classList.remove('show');
         }
       }
     }
-  }
+}
+
+
+
+
+
 
 addEventListener("keydown", function()
 {
@@ -123,6 +197,7 @@ pad1.addEventListener("mouseup", function()
     note.innerHTML = textbox.value;
     note1.appendChild(note);
     textbox.value = "";
+    setCookie("info1",note.innerHTML,30);
     readyToDelete1 = false;
     readyToChange = false;
 });
@@ -137,6 +212,7 @@ pad2.addEventListener("mouseup", function()
     note.innerHTML = textbox.value;
     note2.appendChild(note);
     textbox.value = "";
+    setCookie("info2",note.innerHTML,30);
     readyToDelete2 = false;
 });
 pad2.addEventListener("mousedown", function()
@@ -149,6 +225,7 @@ pad3.addEventListener("mouseup", function()
     note.innerHTML = textbox.value;
     note3.appendChild(note);
     textbox.value = "";
+    setCookie("info3",note.innerHTML,30);
     readyToDelete3 = false;
 });
 pad3.addEventListener("mousedown", function()
@@ -161,6 +238,7 @@ pad4.addEventListener("mouseup", function()
     note.innerHTML = textbox.value;
     note4.appendChild(note);
     textbox.value = "";
+    setCookie("info4",note.innerHTML,30);
     readyToDelete4 = false;
 });
 pad4.addEventListener("mousedown", function()
@@ -173,6 +251,7 @@ pad5.addEventListener("mouseup", function()
     note.innerHTML = textbox.value;
     note5.appendChild(note);
     textbox.value = "";
+    setCookie("info5",note.innerHTML,30);
     readyToDelete5 = false;
 });
 pad5.addEventListener("mousedown", function()
@@ -185,6 +264,7 @@ pad6.addEventListener("mouseup", function()
     note.innerHTML = textbox.value;
     note6.appendChild(note);
     textbox.value = "";
+    setCookie("info6",note.innerHTML,30);
     readyToDelete6 = false;
 });
 pad6.addEventListener("mousedown", function()
@@ -198,6 +278,7 @@ pad7.addEventListener("mouseup", function()
     note.innerHTML = textbox.value;
     note7.appendChild(note);
     textbox.value = "";
+    setCookie("info7",note.innerHTML,30);
     readyToDelete7 = false;
 });
 pad7.addEventListener("mousedown", function()
@@ -210,6 +291,7 @@ pad8.addEventListener("mouseup", function()
     note.innerHTML = textbox.value;
     note8.appendChild(note);
     textbox.value = "";
+    setCookie("info8",note.innerHTML,30);
     readyToDelete8 = false;
 });
 pad8.addEventListener("mousedown", function()
